7c0853f4d39b51486018059dc1a5ffda8f92f090
Abigail McPhillips <abigail.mcphillips@gmail.com>
2023-05-25 14:27:34 +0100

    Add UK postcode validation
    - Add custom validator that uses the uk_postcode gem to check that
      a postcode looks like a UK postcode
    - Move translation to a generic location so that validator can be
      used on personal address fields later

--- app/models/applicants/employment_detail.rb
+++ app/models/applicants/employment_detail.rb
@@ -6,7 +6,7 @@ module Applicants
     attr_accessor :school_name, :school_postcode, :school_headteacher_name
 
     validates :school_name, presence: true
-    validates :school_postcode, presence: true
+    validates :school_postcode, presence: true, postcode: true
     validates :school_headteacher_name, presence: true
   end
 end
