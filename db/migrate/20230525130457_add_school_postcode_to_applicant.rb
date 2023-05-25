cde185e27b46af1cb405d313b128220ac2d33a97
Abigail McPhillips <abigail.mcphillips@gmail.com>
2023-05-25 14:14:15 +0100

    Add school postcode to form
    - Add migration to add field to applicant
    - Add new form field
    - Add presence validation

---
+++ db/migrate/20230525130457_add_school_postcode_to_applicant.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true 
+
+class AddSchoolPostcodeToApplicant < ActiveRecord::Migration[7.0]
+  def change
+    add_column :applicants, :school_postcode, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_02_121235) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_25_130457) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_02_121235) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "application_route"
+    t.string "school_postcode"
   end
 
 end
