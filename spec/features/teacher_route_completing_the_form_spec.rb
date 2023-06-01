9bf164bebbc550230c0c19ffecebb0d57c01c1b0
Pablo M <pablo@advera.co.uk>
2023-06-01 18:10:12 +0100

    Fix typo when asserting outcome in system test

--- spec/features/teacher_route_completing_the_form_spec.rb
+++ spec/features/teacher_route_completing_the_form_spec.rb
@@ -18,8 +18,8 @@ describe "teacher route: completing the form" do
       and_i_enter_my_entry_date
       and_i_enter_my_personal_details
       and_i_enter_my_employment_details
-      
-      expect(page).to have_text("hank you for completing the international relocation payment application form")
+
+      expect(page).to have_text("Thank you for completing the international relocation payment application form")
     end
   end
 end
