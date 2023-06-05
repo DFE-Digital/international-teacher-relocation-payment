8fe29f51911e8e0e448fbc370ba8791117f7fb68
Pablo M <pablo@advera.co.uk>
2023-06-05 17:28:06 +0100

    Create validator for Dates: year, month, day
    During the journey when a date is requested for the user, we always ask for the
    year, month and day. These three attributes are mandatory in order to proceed to
    the next question.
    
    In the current implementation we are using a date helper that performs the
    validation; I have decided to extract this out into a validator as seems a bit
    more aligned with the ActiveModel lifecycle.
    
    This is also aligned with how the MVP was implemented for the email validator.

---
+++ spec/validators/day_month_year_date_validator_spec.rb
@@ -0,0 +1,69 @@
+# spec/validators/day_month_year_date_validator_spec.rb
+
+require "rails_helper"
+
+RSpec.describe DayMonthYearDateValidator do
+  class Validatable
+    include ActiveModel::Model
+    attr_accessor :day, :month, :year
+
+    validate do |record|
+      DayMonthYearDateValidator.new.validate(record)
+    end
+  end
+
+  subject(:validatable) { Validatable.new }
+
+  describe "#validate" do
+    context "when the date is valid" do
+      it "passes validation" do
+        validatable.day = 15
+        validatable.month = 6
+        validatable.year = 2023
+        validatable.valid?
+
+        expect(validatable).to be_valid
+      end
+    end
+
+    context "when the date is invalid" do
+      it "fails validation" do
+        validatable.day = 31
+        validatable.month = 2 # Invalid month
+        validatable.year = 2023
+        validatable.valid?
+
+        expect(validatable.errors[:day_month_year_date]).to include("is not a valid date")
+      end
+    end
+
+    context "when missing fields" do
+      before do
+        validatable.day = 15
+        validatable.month = 6
+        validatable.year = 2023
+      end
+
+      it "fails validation when missing day" do
+        validatable.day = nil
+        validatable.valid?
+
+        expect(validatable.errors[:day_month_year_date]).to include("is not a valid date")
+      end
+
+      it "fails validation when missing month" do
+        validatable.month = nil
+        validatable.valid?
+
+        expect(validatable.errors[:day_month_year_date]).to include("is not a valid date")
+      end
+
+      it "fails validation when missing year" do
+        validatable.year = nil
+        validatable.valid?
+
+        expect(validatable.errors[:day_month_year_date]).to include("is not a valid date")
+      end
+    end
+  end
+end
