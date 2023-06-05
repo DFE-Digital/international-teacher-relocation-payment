80e3676a91fcdd0c240e2393757bf537eae96e1c
Pablo M <pablo@advera.co.uk>
2023-06-05 18:06:03 +0100

    Validator accepts also String as attributes
    This is the expected behaviour as the values passed to the model will usually be
    Strings.

--- spec/validators/day_month_year_date_validator_spec.rb
+++ spec/validators/day_month_year_date_validator_spec.rb
@@ -12,8 +12,9 @@ RSpec.describe DayMonthYearDateValidator do
     end
   end
 
-  let(:valid_attributes) { { day: 15, month: 6, year: 2023 } }
   subject(:validatable) { Validatable.new(valid_attributes) }
+
+  let(:valid_attributes) { { day: "15", month: "6", year: "2023" } }
 
   describe "#validate" do
     context "when the date is valid" do
@@ -26,8 +27,8 @@ RSpec.describe DayMonthYearDateValidator do
 
     context "when the date is invalid" do
       it "fails validation" do
-        validatable.day = 31
-        validatable.month = 2 # Invalid month
+        validatable.day = "31"
+        validatable.month = "2" # Invalid month
         validatable.valid?
 
         expect(validatable.errors[:day_month_year_date]).to include("Enter a valid date")
