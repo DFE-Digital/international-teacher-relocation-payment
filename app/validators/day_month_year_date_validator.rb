9fc0f2e86b045a88b9789a53e9504fa5327cb3b7
Pablo M <pablo@advera.co.uk>
2023-06-05 17:28:06 +0100

    Create validator for Dates: year, month, day
    During the journey when a date is requested for the user, we always ask for the
    year, month and day. These three attributes are mandatory in order to proceed to
    the next question.
    
    In the current implementation we are using a date helper that performs the
    validation; I have decided to extract this out into a validator as seems a bit
    more aligned with the ActiveModel lifecycle.
    
    This is also aligned with how the MVP was implemented for the email validator.

---
+++ app/validators/day_month_year_date_validator.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class DayMonthYearDateValidator
+  def validate(record, attribute = "day_month_year_date")
+    @record = record
+
+    record.errors.add(attribute, "is not a valid date") unless valid?
+  end
+
+private
+
+  def valid?
+    day = @record.day
+    month = @record.month
+    year = @record.year
+
+    return false unless day.present? && month.present? && year.present?
+
+    Date.valid_date?(year, month, day)
+  rescue ArgumentError
+    false
+  end
+end
