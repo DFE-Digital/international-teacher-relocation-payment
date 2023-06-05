cc3259d28b3306784b576480bd0c81bf7dc9c043
Pablo M <pablo@advera.co.uk>
2023-06-05 09:41:12 +0100

    Make explicit use of Subject postcode via `let`
    This seems like a more idiomatic approach to the use of `subject`.

--- spec/validators/postcode_validator_spec.rb
+++ spec/validators/postcode_validator_spec.rb
@@ -3,17 +3,15 @@
 require "rails_helper"
 
 describe PostcodeValidator do
-  subject { Applicants::EmploymentDetail.new }
-
-  before do
-    subject.school_postcode = postcode
-  end
+  subject(:applicant) { Applicants::EmploymentDetail.new(school_postcode: postcode) }
 
   context "with a valid UK postcode" do
     let(:postcode) { "SW1A 1AA" }
 
     it "does not add an error" do
-      expect(subject.errors[:school_postcode]).to be_blank
+      applicant.valid?
+
+      expect(applicant.errors[:school_postcode]).to be_blank
     end
   end
 
@@ -21,8 +19,9 @@ describe PostcodeValidator do
     let(:postcode) { "not really a postcode" }
 
     it "adds an error" do
-      expect(subject).not_to be_valid
-      expect(subject.errors[:school_postcode]).not_to be_blank
+      applicant.valid?
+
+      expect(applicant.errors[:school_postcode]).not_to be_blank
     end
   end
 end
