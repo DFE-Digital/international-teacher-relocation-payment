804afbcac99e55678ae89bffa00b2f5a06e0f6ed
Pablo M <pablo@advera.co.uk>
2023-06-09 13:50:46 +0100

    Add documentation to InvalidDate
    This class is not obvious to understand; it is only when you try to integrate a
    failing date into the GOV.UK date component when it clicks.
    
    I am hoping that this documentation will help future developers.

--- app/models/invalid_date.rb
+++ app/models/invalid_date.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# The InvalidDate class is an ActiveModel model used for situations when
+# the date provided is not valid. As per the current implementation this
+# date is passed to the view so it is rendered as a normal date via the
+# attribues: `day`, `month` and `year`.
+#
+# This same approach is used in the following places across Dfe:
+#   - https://github.com/DFE-Digital/register-trainee-teachers
+#   - https://github.com/DFE-Digital/early-careers-framework
+#
+# @attr_accessor [Integer] day The day component of the date.
+# @attr_accessor [Integer] month The month component of the date.
+# @attr_accessor [Integer] year The year component of the date.
 class InvalidDate
   include ActiveModel::Model
 
