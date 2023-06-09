18fac07a2c9ccb86ed0cbbd7545ea92e3287f8eb
Pablo M <pablo@advera.co.uk>
2023-06-09 13:16:36 +0100

    Extract class InvalidDate
    InvalidDate encapsulate a Date with three attributes: day, month and year, but
    that is not valid. For example, February 31st
    
    The benefit of using this class is how easy it is the integration with the
    GOV.UK component for dates.

--- app/models/applicants/contract_start_date.rb
+++ app/models/applicants/contract_start_date.rb
@@ -16,15 +16,5 @@ module Applicants
     rescue StandardError
       InvalidDate.new(day:, month:, year:)
     end
-
-    InvalidDate = Struct.new(:day, :month, :year, keyword_init: true) do
-      def blank?
-        members.any? { |date_field| public_send(date_field).blank? }
-      end
-
-      def present?
-        false
-      end
-    end
   end
 end
