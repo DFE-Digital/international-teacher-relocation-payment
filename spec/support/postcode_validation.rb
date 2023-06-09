75e8876bee58b3ebfc57781204ef9eeee5ef4181
Pablo M <pablo@advera.co.uk>
2023-06-09 16:32:08 +0100

    Add shared examples for postcode validations for Schools

--- spec/support/postcode_validation.rb
+++ spec/support/postcode_validation.rb
@@ -1,15 +1,15 @@
-RSpec.shared_examples "a valid UK postcode" do |klass|
+RSpec.shared_examples "a valid UK postcode" do |klass, attribute_name = :postcode|
   it "accepts valid postcodes" do
-    model = klass.new(postcode: "SW1A 1AA")
+    model = klass.new(attribute_name => "SW1A 1AA")
     model.valid?
 
-    expect(model.errors[:postcode]).to be_empty
+    expect(model.errors[attribute_name]).to be_empty
   end
 
   it "rejects invalid postcodes" do
-    model = klass.new(postcode: "Invalid postcode")
+    model = klass.new(attribute_name => "Invalid postcode")
     model.valid?
 
-    expect(model.errors[:postcode]).to include("Enter a valid postcode (for example, BN1 1AA)")
+    expect(model.errors[attribute_name]).to include("Enter a valid postcode (for example, BN1 1AA)")
   end
 end
