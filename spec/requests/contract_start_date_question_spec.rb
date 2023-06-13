8bcace8ded73cc0a9f73324b29cb341949491d5f
Pablo M <pablo@advera.co.uk>
2023-06-13 14:37:24 +0100

    Replace controller test with request test
    This seems more aligned with Dfe ways of testing. This commit introduces a mock
    on the controller session that needs to be replaced once we move out of session
    storage for user attributes.

---
+++ spec/requests/contract_start_date_question_spec.rb
@@ -0,0 +1,78 @@
+require "rails_helper"
+
+module Applicants
+  RSpec.describe ContractStartDatesController do
+    describe "POST create" do
+      let(:valid_params) do
+        {
+          applicants_contract_start_date: {
+            "contract_start_date(3i)" => "1",
+            "contract_start_date(2i)" => "1",
+            "contract_start_date(1i)" => "2023",
+          },
+        }
+      end
+
+      context "when it is not a teacher" do
+        before do
+          set_user_type("salaried_trainee")
+        end
+
+        it "redirects to the start of the process" do
+          post "/applicants/contract_start_dates", params: valid_params
+
+          expect(response).to redirect_to(new_applicants_application_route_path)
+        end
+      end
+
+      context "when it is a teacher" do
+        before do
+          set_user_type("teacher")
+        end
+
+        context "with valid params" do
+          it "redirects to the new subject path if it is eligible" do
+            allow(Policies::ContractStartDate).to receive(:eligible?).and_return(true)
+            post "/applicants/contract_start_dates", params: valid_params
+
+            expect(response).to redirect_to(new_applicants_subject_path)
+          end
+
+          it "redirects to the ineligible path if it is not eligible" do
+            allow(Policies::ContractStartDate).to receive(:eligible?).and_return(false)
+            post "/applicants/contract_start_dates", params: valid_params
+
+            expect(response).to redirect_to(ineligible_path)
+          end
+        end
+
+        context "with invalid params" do
+          let(:invalid) do
+            {
+              applicants_contract_start_date: {
+                "contract_start_date(3i)" => "31",
+                "contract_start_date(2i)" => "31",
+                "contract_start_date(1i)" => "2023",
+              },
+            }
+          end
+
+          it "renders the new template" do
+            post "/applicants/contract_start_dates", params: invalid
+
+            expect(response.body).to include('Enter the start date of your contract')
+          end
+        end
+      end
+
+      def set_user_type(route_type)
+        # TODO: Remove this stub when we have a better way of storing the route type on the DB
+        # The current implementation of the application route is to store it in the session,
+        # this is not ideal and should be changed to be stored in the database. For now we are
+        # stubbing the session to return the route type, but this is temporary.
+        allow_any_instance_of(described_class).to receive(:session).and_return({ "application_route" => route_type })
+      end
+    end
+
+  end
+end
