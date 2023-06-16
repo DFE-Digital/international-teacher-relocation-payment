92347f199e3963f695ad73711bfc30909ec4f15f
Pablo M <pablo@advera.co.uk>
2023-06-16 13:09:17 +0100

    Add validation to the Address
    I still need to add the postcode validation, but I would rather doing it
    separately as it also affects applicants.

---
+++ spec/models/address.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Address do
+  it { is_expected.to belong_to(:addressable) }
+
+  it { is_expected.to validate_presence_of(:address_line_1) }
+  it { is_expected.to validate_presence_of(:city) }
+  it { is_expected.to validate_presence_of(:postcode) }
+end
