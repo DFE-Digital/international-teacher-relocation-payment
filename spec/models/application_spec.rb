00e90cef924d4341ee70f4e8b490d44321d6ad20
Pablo M <pablo@advera.co.uk>
2023-06-19 16:15:56 +0100

    Add Urn to the Application model

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -16,6 +16,17 @@
 require "rails_helper"
 
 RSpec.describe Application do
-  it { is_expected.to validate_presence_of(:urn) }
   it { is_expected.to validate_presence_of(:application_date) }
+
+  describe "#urn" do
+    subject(:application) { described_class.new }
+
+    it "is not blank on instantiation" do
+      expect(application.urn).to be_present
+    end
+
+    it "starts with IRP" do
+      expect(application.urn.value).to match(/\AIRP [ABCFHJKLMNPRSTUV23456789]{6}\z/)
+    end
+  end
 end
