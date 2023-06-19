9b6b6936c282dce0ae1a710651c894a1d2161dd3
Pablo M <pablo@advera.co.uk>
2023-06-19 19:19:20 +0100

    Remove Applicant dependency from submission view
    The submission is related to an application not to an applicant. This seems like
    a legit update to match the current domain.

--- app/controllers/applicants/submissions_controller.rb
+++ app/controllers/applicants/submissions_controller.rb
@@ -3,7 +3,9 @@
 module Applicants
   class SubmissionsController < ApplicationController
     def show
-      @applicant = Applicant.find(session[:applicant_id])
+      applicant = Applicant.find(session[:applicant_id])
+
+      @application = applicant.application
     end
   end
 end
