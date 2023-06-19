01a948481c611bba9d45e01480457fcc2a6575dd
Pablo M <pablo@advera.co.uk>
2023-06-19 12:41:09 +0100

    Add Application domain

---
+++ spec/factories/applications.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: applications
+#
+#  id               :bigint           not null, primary key
+#  application_date :string           not null
+#  urn              :string           not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  applicant_id     :bigint           not null
+#
+# Foreign Keys
+#
+#  fk_rails_...  (applicant_id => applicants.id)
+#
+FactoryBot.define do
+  factory :application do
+    application_date { Faker::Date.in_date_period }
+    urn { "IRP #{Faker::Number.number(digits: 6)} LT" }
+    applicant
+  end
+end
