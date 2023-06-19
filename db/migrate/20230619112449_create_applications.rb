8075ae4ecf24b2377278feea2e6941b50f606428
Pablo M <pablo@advera.co.uk>
2023-06-19 21:19:57 +0100

    Update schema for Application: use Date for application_date

--- db/migrate/20230619112449_create_applications.rb
+++ db/migrate/20230619112449_create_applications.rb
@@ -1,7 +1,7 @@
 class CreateApplications < ActiveRecord::Migration[7.0]
   def change
     create_table :applications do |t|
-      t.string :application_date, null: false
+      t.date :application_date, null: false
       t.string :urn, null: false
       t.references :applicant, null: false, foreign_key: true
 
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: addresses
+#
+#  id               :bigint           not null, primary key
+#  address_line_1   :string
+#  address_line_2   :string
+#  addressable_type :string           not null
+#  city             :string
+#  county           :string
+#  postcode         :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  addressable_id   :bigint           not null
+#
 require "rails_helper"
 
 describe Address do
