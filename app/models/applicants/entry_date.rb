e6be6e17687b3cb4406ce84b03e7891b53690ac5
Pablo M <pablo@advera.co.uk>
2023-06-21 13:35:16 +0100

    Entry date applies for both routes
    We added the Start Date of contract for the Trainee, which opened the need to
    enable the eligibility rule for both types of applicants.

--- app/models/applicants/entry_date.rb
+++ app/models/applicants/entry_date.rb
@@ -22,8 +22,6 @@ module Applicants
     # If the applicant is a teacher who entered the country more than three
     # months before their contract start date, they are not eligible.
     def eligible?(session)
-      return true if session["application_route"] == "salaried_trainee"
-
       entry_date >= Date.parse(session["contract_start_date"]) - 3.months
     end
   end
