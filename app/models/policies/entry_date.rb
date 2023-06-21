a98a7dc43658942aab4e3d682e7111b9425a4f89
Pablo M <pablo@advera.co.uk>
2023-06-21 13:50:51 +0100

    Introduce EntryDate policy
    The `EntryDate` class defines the policy for contract start dates.
    
    This Policy determines whether a given entry date is eligible based on the
    start date of the contract. If the applicant entered the country 3 months
    or less before the start of the contract, then they are eligible.
    
    ```ruby
    Policies::ContractStartDate.eligible?(
      Date.new(2023, 1, 2),
      Date.new(2023, 4, 2)
    ) #=> true
    ```

---
+++ app/models/policies/entry_date.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# The `EntryDate` class defines the policy for contract start dates.
+#
+# This Policy determines whether a given entry date is eligible based on the
+# start date of the contract. If the applicant entered the country 3 months
+# or less before the start of the contract, then they are eligible.
+#
+# @example
+#   Policies::ContractStartDate.eligible?(
+#     Date.new(2023, 1, 2),
+#     Date.new(2023, 4, 2)
+#   ) #=> true
+#
+module Policies
+  class EntryDate
+    def eligible?(entry_date, start_date)
+      entry_date >= start_date - 3.months
+    end
+  end
+end
