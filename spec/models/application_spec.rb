21b46edaae784ec3321fad3565c5710f063e5c77
Pablo M <pablo@advera.co.uk>
2023-06-22 11:07:22 +0100

    Use factory method to create URNs for applications
    On this commit I am using inheritance for the factories, as they make a bit
    clearer the type of object that we are creating.

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -19,14 +19,28 @@ RSpec.describe Application do
   it { is_expected.to validate_presence_of(:application_date) }
 
   describe "#urn" do
-    subject(:application) { described_class.new }
+    it "is blank before creation" do
+      application = build(:teacher_application)
 
-    it "is not blank on instantiation" do
+      expect(application.urn).to be_blank
+    end
+
+    it "is present after creation" do
+      application = create(:teacher_application)
+
       expect(application.urn).to be_present
     end
 
-    it "starts with IRP" do
-      expect(application.urn.value).to match(/\AIRP[ABCDEFHJKLMNPRSTUV23456789]{6}\z/)
+    it "matches the required format for a teacher" do
+      application = create(:teacher_application)
+
+      expect(application.urn).to match(/^IRPTE[A-Z0-9]{6}$/)
+    end
+
+    it "matches the required format for a trainee" do
+      application = create(:salaried_trainee_application)
+
+      expect(application.urn).to match(/^IRPLT[A-Z0-9]{6}$/)
     end
   end
 
