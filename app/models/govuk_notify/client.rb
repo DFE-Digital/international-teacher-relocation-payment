a4ff22c19c4c1ab446726556cefb8876a6ac0313
Pablo M <pablo@advera.co.uk>
2023-06-27 21:12:43 +0100

    Add client to send emails to applicants
    The only parameter that is dependent of the user is the `applicant_id` which is
    displyed in the message.

---
+++ app/models/govuk_notify/client.rb
@@ -0,0 +1,19 @@
+module GovukNotify
+  class Client
+    def self.send_email(template_id, email_address, application_id)
+      new.send_email(template_id, email_address, application_id)
+    end
+
+    def initialize
+      @client = Notifications::Client.new(ENV.fetch("GOVUK_NOTIFY_API_KEY", nil))
+    end
+
+    def send_email(template_id, email_address, application_id)
+      @client.send_email(
+        template_id:,
+        email_address:,
+        application_id:,
+      )
+    end
+  end
+end
