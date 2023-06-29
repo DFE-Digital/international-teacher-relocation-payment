803d2632ec0f33c50f69fe2dc4fb66ca9889430f
Pablo M <pablo@advera.co.uk>
2023-06-29 17:09:24 +0200

    Use Application as the root entity for Application factories
    I am trying to make the factories more expressive, so when we create a factory
    that is in an initial state, we created precisely that:
    
    ```ruby
    create :application, :with_initial_checks_completed
    ```
    
    This reads a bit better and enables a single point for application creation. It
    is also removing the `traits` at the `application_progress` level for creating
    teachers and trainees.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,14 @@
 Rails.logger.debug("Deleting all applicants and applicant progresses...")
 Applicant.destroy_all if Rails.env.development?
 
-%i[with_salaried_trainee with_teacher].each do |route|
-  Rails.logger.debug { "Creating #{route} applicants..." }
-  FactoryBot.create_list(:application_progress, 5, :with_initial_checks_completed, route)
-  FactoryBot.create_list(:application_progress, 5, :with_visa_investigation_required, route)
-  FactoryBot.create_list(:application_progress, 5, :with_home_office_checks_completed, route)
-  FactoryBot.create_list(:application_progress, 5, :with_school_investigation_required, route)
-  FactoryBot.create_list(:application_progress, 5, :with_school_checks_completed, route)
-end
+FactoryBot.create_list(:teacher_application, 5, :with_initial_checks_completed)
+FactoryBot.create_list(:teacher_application, 5, :with_visa_investigation_required)
+FactoryBot.create_list(:teacher_application, 5, :with_home_office_checks_completed)
+FactoryBot.create_list(:teacher_application, 5, :with_school_investigation_required)
+FactoryBot.create_list(:teacher_application, 5, :with_school_checks_completed)
+
+FactoryBot.create_list(:salaried_trainee_application, 5, :with_initial_checks_completed)
+FactoryBot.create_list(:salaried_trainee_application, 5, :with_visa_investigation_required)
+FactoryBot.create_list(:salaried_trainee_application, 5, :with_home_office_checks_completed)
+FactoryBot.create_list(:salaried_trainee_application, 5, :with_school_investigation_required)
+FactoryBot.create_list(:salaried_trainee_application, 5, :with_school_checks_completed)
