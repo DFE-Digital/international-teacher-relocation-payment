84d60b4910a23e4ebe64f76ce3a18dfb9dea036d
Pablo M <pablo@advera.co.uk>
2023-06-30 08:34:31 +0200

    Extends healthchecks request tests

--- spec/requests/healthchecks_spec.rb
+++ spec/requests/healthchecks_spec.rb
@@ -1,8 +1,14 @@
 require "rails_helper"
 
-RSpec.describe "Healthcheck" do
-  it "responds successfully" do
+RSpec.describe "Healthchecks" do
+  it "default request responds successfully when the rails server is up" do
     get "/healthcheck"
+
+    expect(response).to have_http_status(:ok)
+  end
+
+  it "database checkpoint responds successfully when database is up" do
+    get "/healthcheck/database"
 
     expect(response).to have_http_status(:ok)
   end
