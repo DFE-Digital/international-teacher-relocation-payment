c7313b42df3f18722c69eb1a30d84c62fc15ae5a
Pablo M <pablo@advera.co.uk>
2023-06-30 11:00:19 +0200

    Remove three states for boolean values in database
    This has been detected by the upgrade of Rubocop, and it is a sensible thing not
    to have `nil` values on boolean fields.

--- app/models/application_progress.rb
+++ app/models/application_progress.rb
@@ -10,8 +10,8 @@
 #  payment_completed_at            :date
 #  rejection_completed_at          :date
 #  school_checks_completed_at      :date
-#  school_investigation_required   :boolean
-#  visa_investigation_required     :boolean
+#  school_investigation_required   :boolean          default(FALSE), not null
+#  visa_investigation_required     :boolean          default(FALSE), not null
 #  created_at                      :datetime         not null
 #  updated_at                      :datetime         not null
 #  application_id                  :bigint
