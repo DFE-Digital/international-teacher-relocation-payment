e14e32fe9d6216ec3211383b168ae62f9df6b583
Raul Gracia <raulgracialario@gmail.com>
2023-07-04 21:02:45 +0100

    Small fixes from qa feedback (#117)
    * Small fixes from qa feedback
    
    * Add test for the visa breakdown

--- spec/features/admin_console/dashboard_spec.rb
+++ spec/features/admin_console/dashboard_spec.rb
@@ -99,9 +99,10 @@ describe "Dashboard" do
   end
 
   def given_there_are_few_applications_with_visas
-    create(:applicant, visa_type: Applicants::Visa::VISA_OPTIONS[0])
-    create(:applicant, visa_type: Applicants::Visa::VISA_OPTIONS[1])
-    create(:applicant, visa_type: Applicants::Visa::VISA_OPTIONS[2])
+    create_list(:applicant, 4, visa_type: Applicants::Visa::VISA_OPTIONS[0])
+    create_list(:applicant, 3, visa_type: Applicants::Visa::VISA_OPTIONS[1])
+    create_list(:applicant, 2, visa_type: Applicants::Visa::VISA_OPTIONS[2])
+    create(:applicant, visa_type: Applicants::Visa::VISA_OPTIONS[3])
   end
 
   def given_there_are_rejected_applications
@@ -151,7 +152,7 @@ describe "Dashboard" do
 
   def then_i_can_see_the_total_paid_widget
     within ".kpi-widget.paid" do
-      expect(page).to have_content("Total Paid")
+      expect(page).to have_content("Payment Confirmations")
       expect(page).to have_content("2")
     end
   end
@@ -182,7 +183,10 @@ describe "Dashboard" do
       expect(page).to have_content(Applicants::Visa::VISA_OPTIONS[0])
       expect(page).to have_content(Applicants::Visa::VISA_OPTIONS[1])
       expect(page).to have_content(Applicants::Visa::VISA_OPTIONS[2])
-      expect(page).to have_content("1")
+      expect(page).not_to have_content(Applicants::Visa::VISA_OPTIONS[3])
+      expect(page).to have_content("4")
+      expect(page).to have_content("3")
+      expect(page).to have_content("2")
     end
   end
 
