0771fa41c8b5c2b3aec086e345642f4a31c6ebe8
Pablo M <pablo@advera.co.uk>
2023-07-07 12:40:32 +0200

    Add feature tests for trainee: backwards navigation

--- spec/features/trainee_route_completing_the_form_spec.rb
+++ spec/features/trainee_route_completing_the_form_spec.rb
@@ -33,4 +33,40 @@ describe "trainee route: completing the form" do
       end
     end
   end
+
+  describe "navigating backwards" do
+    it "allows the user to navigate back to the previous pages" do
+      when_i_start_the_form
+      and_i_complete_application_route_question_with(option: "salaried_trainee")
+      and_i_complete_the_trainee_employment_conditions
+      and_i_enter_my_contract_start_date
+      and_i_select_my_subject("salaried_trainee")
+      and_i_select_my_visa_type
+      and_i_enter_my_entry_date("salaried_trainee")
+      and_i_enter_my_personal_details
+
+      # We're now on the employment details page
+      # We start going backwards
+      when_i_click_the_back_link
+      assert_i_am_in_the_personal_details_question
+
+      when_i_click_the_back_link
+      assert_i_am_in_the_entry_date_question("salaried_trainee")
+
+      when_i_click_the_back_link
+      assert_i_am_in_the_visa_type_question
+
+      when_i_click_the_back_link
+      assert_i_am_in_the_subject_question("salaried_trainee")
+
+      when_i_click_the_back_link
+      assert_i_am_in_the_contract_start_date_question
+
+      when_i_click_the_back_link
+      assert_i_am_in_the_trainee_employment_conditions_question
+
+      when_i_click_the_back_link
+      assert_i_am_in_the_application_route_question
+    end
+  end
 end
