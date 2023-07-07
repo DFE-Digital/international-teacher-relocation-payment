2f3b3a8df3e7edad04c169e8fa60af006bdcc319
Pablo M <pablo@advera.co.uk>
2023-07-07 11:41:48 +0200

    Add system test for backwards navigation for teacher

--- spec/features/teacher_route_completing_the_form_spec.rb
+++ spec/features/teacher_route_completing_the_form_spec.rb
@@ -35,4 +35,42 @@ describe "teacher route: completing the form" do
       end
     end
   end
+
+  describe "navigating backwards" do
+    it "allows the user to navigate back to the previous pages" do
+      when_i_start_the_form
+      and_i_complete_application_route_question_with(option: "teacher")
+      and_i_complete_the_state_school_question
+      and_i_complete_the_contract_details_question
+      and_i_enter_my_contract_start_date
+      and_i_select_my_subject("teacher")
+      and_i_select_my_visa_type
+      and_i_enter_my_entry_date("teacher")
+      and_i_enter_my_personal_details
+
+      when_i_click_the_back_link
+      assert_i_am_in_the_personal_details_question
+
+      when_i_click_the_back_link
+      assert_i_am_in_the_entry_date_question("teacher")
+
+      when_i_click_the_back_link
+      assert_i_am_in_the_visa_type_question
+
+      when_i_click_the_back_link
+      assert_i_am_in_the_subject_question("teacher")
+
+      when_i_click_the_back_link
+      assert_i_am_in_the_contract_start_date_question
+
+      when_i_click_the_back_link
+      assert_i_am_on_the_contract_details_question
+
+      when_i_click_the_back_link
+      assert_i_am_in_the_state_school_question
+
+      when_i_click_the_back_link
+      assert_i_am_in_the_application_route_question
+    end
+  end
 end
