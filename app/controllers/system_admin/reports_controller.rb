1e06d01c1592d7bb056b6ccafff4ca6a14972ffc
Pablo M <pablo@advera.co.uk>
2023-07-11 20:39:50 +0200

    Move CSV data into the Report
    We should not have the content on the controller, but on the report.

--- app/controllers/system_admin/reports_controller.rb
+++ app/controllers/system_admin/reports_controller.rb
@@ -6,16 +6,7 @@ module SystemAdmin
       report = Reports::HomeOffice.new
       headers["Content-Type"] = "text/csv"
 
-      send_data(csv_data, filename: report.name)
-    end
-
-    def csv_data
-      # Generate your CSV data using the CSV module or any other method
-      CSV.generate do |csv|
-        # Add your CSV content here
-        csv << ["Header 1", "Header 2", "Header 3"]
-        csv << ["Data 1", "Data 2", "Data 3"]
-      end
+      send_data(report.data, filename: report.name)
     end
   end
 end
--- app/models/reports/home_office.rb
+++ app/models/reports/home_office.rb
@@ -5,5 +5,13 @@ module Reports
     def name
       "Home-Office-Report.csv"
     end
+
+    def data
+      CSV.generate do |csv|
+        # Add your CSV content here
+        csv << ["Header 1", "Header 2", "Header 3"]
+        csv << ["Data 1", "Data 2", "Data 3"]
+      end
+    end
   end
 end
