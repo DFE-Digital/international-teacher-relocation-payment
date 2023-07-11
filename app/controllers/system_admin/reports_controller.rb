c6f8295eefb3deca2fa607f0b9cf9fe055b590fa
Pablo M <pablo@advera.co.uk>
2023-07-11 20:23:54 +0200

    Download CSV for Home Office
    Content is still pending, but I am exercising outside-in an agent downloading
    the CSV.

--- app/controllers/system_admin/reports_controller.rb
+++ app/controllers/system_admin/reports_controller.rb
@@ -1,6 +1,21 @@
 module SystemAdmin
   class ReportsController < AdminController
-    def index
+    def index; end
+
+    def show
+        headers["Content-Type"] = "text/csv"
+        headers["Content-Disposition"] = 'attachment; filename="Home-Office-Report.csv"'
+
+        send_data(csv_data, filename: 'Home-Office-Report.csv')
+    end
+
+    def csv_data
+      # Generate your CSV data using the CSV module or any other method
+      CSV.generate do |csv|
+        # Add your CSV content here
+        csv << ["Header 1", "Header 2", "Header 3"]
+        csv << ["Data 1", "Data 2", "Data 3"]
+      end
     end
   end
 end
--- spec/features/admin_console/reports_spec.rb
+++ spec/features/admin_console/reports_spec.rb
@@ -8,6 +8,16 @@ describe "Reports - export to CSV" do
   it "exports Home Office CSV" do
     given_i_am_signed_as_an_admin
     when_i_am_in_the_reports_page
+
+    and_i_click_on_the_home_office_csv_link
+
+    expect(page.response_headers["Content-Type"]).to match /text\/csv/
+    expect(page.response_headers["Content-Disposition"]).to include "attachment"
+    expect(page.response_headers["Content-Disposition"]).to include 'filename="Home-Office-Report.csv"'
+  end
+
+  def and_i_click_on_the_home_office_csv_link
+    click_on "Download home office report"
   end
 
   def when_i_am_in_the_reports_page
