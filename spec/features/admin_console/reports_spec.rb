3be5016e6e381556e520e2400eb238e16bee2a67
Pablo M <pablo@advera.co.uk>
2023-07-11 23:30:32 +0200

    Extract method for checking the downloaded file

--- spec/features/admin_console/reports_spec.rb
+++ spec/features/admin_console/reports_spec.rb
@@ -7,10 +7,17 @@ describe "Reports - export to CSV" do
 
   it "exports Home Office CSV" do
     given_i_am_signed_as_an_admin
+
     when_i_am_in_the_reports_page
 
     and_i_click_on_the_home_office_csv_link
 
+    then_the_home_office_csv_report_is_downloaded
+  end
+
+private
+
+  def then_the_home_office_csv_report_is_downloaded
     expect(page.response_headers["Content-Type"]).to match(/text\/csv/)
     expect(page.response_headers["Content-Disposition"]).to include "attachment"
     expect(page.response_headers["Content-Disposition"]).to include 'filename="Home-Office-Report.csv"'
