131b26441a8e1d518d83a315ba57051290d97453
Pablo M <pablo@advera.co.uk>
2023-07-13 10:39:11 +0200

    Implement open window based on start/end service dates

--- app/models/gatekeeper.rb
+++ app/models/gatekeeper.rb
@@ -1,8 +1,17 @@
-# Enables or disables access to the entire application.
+# frozen_string_literal: true
+
+# Controls access to the application. The application is open when the current
+# date is between the start and end of the service. The start and end of the
+# service are configurable via the AppSettings model.
 #
 class Gatekeeper
   def self.application_open?
     return false if AppSettings.current.service_start_date.nil?
     return false if AppSettings.current.service_end_date.nil?
+
+    Date.current.between?(
+      AppSettings.current.service_start_date,
+      AppSettings.current.service_end_date,
+    )
   end
 end
--- spec/models/gatekeeper_spec.rb
+++ spec/models/gatekeeper_spec.rb
@@ -25,4 +25,32 @@ describe "Gatekeeper" do
 
     expect(Gatekeeper.application_open?).to be(false)
   end
+
+  it "returns true when current date is between start and end of the service" do
+    expect(Gatekeeper.application_open?).to be(true)
+  end
+
+  it "returns false when current date is before start of the service" do
+    AppSettings.current.update!(service_start_date: 1.day.from_now)
+
+    expect(Gatekeeper.application_open?).to be(false)
+  end
+
+  it "returns false when current date is after end of the service" do
+    AppSettings.current.update!(service_end_date: 1.day.ago)
+
+    expect(Gatekeeper.application_open?).to be(false)
+  end
+
+  it "returns true when current date is equal to start of the service" do
+    AppSettings.current.update!(service_start_date: Time.zone.now.to_date)
+
+    expect(Gatekeeper.application_open?).to be(true)
+  end
+
+  it "returns true when current date is equal to end of the service" do
+    AppSettings.current.update!(service_end_date: Time.zone.now.to_date)
+
+    expect(Gatekeeper.application_open?).to be(true)
+  end
 end
